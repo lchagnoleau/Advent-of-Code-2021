fdd7fb5d44f90aa891f833254837ad0ea4487a8f
Loïc Chagnoleau <lchagnoleau@pm.me>
2021-12-01 16:01:05 +0100

    compare and get result

--- Day_1/main.rs
+++ Day_1/main.rs
@@ -2,9 +2,16 @@ use std::fs;
 
 fn main() {
   let v_input = read_input("input.txt");
-  for s in v_input {
-    println!("{}", s)
+
+  let mut nb_of_element_sup_as_prev = 0;
+  for i in 1..v_input.len() {
+    if v_input[i] > v_input[i-1] {
+      nb_of_element_sup_as_prev += 1;
+    }
   }
+
+  println!("Number of numbers : {}", v_input.len());
+  println!("Number of numbers superior as previous : {}", nb_of_element_sup_as_prev);
 }
 
 fn read_input(filename: &str)-> Vec<i32> {
