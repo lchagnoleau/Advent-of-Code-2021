a6837a15876ad403c250f18e2fbeea642e9629e9
Loïc Chagnoleau <lchagnoleau@pm.me>
2021-12-08 23:50:06 +0100

    Day 1 Part 1

---
+++ src/day1_1.rs
@@ -0,0 +1,28 @@
+use std::fs;
+
+pub struct Day1_1 {
+    pub input_file: String
+}
+
+impl Day1_1 {
+    pub fn result(&self) {
+        let v_input = read_input(&self.input_file);
+
+        let mut nb_of_element_sup_as_prev = 0;
+        for i in 1..v_input.len() {
+            if v_input[i] > v_input[i-1] {
+                nb_of_element_sup_as_prev += 1;
+            }
+        }
+        println!("Day 1 Part 1 result : {}", nb_of_element_sup_as_prev);
+    }
+}
+
+fn read_input(filename: &str)-> Vec<i32> {
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Unable to open");
+
+    contents.split('\n').map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use crate::day1_1::Day1_1;
+
+mod day1_1;
+
 fn main() {
-    println!("Hello, world!");
+    let day1_1 = Day1_1 {input_file: String::from("inputs/day1.txt")};
+
+    day1_1.result();
 }
